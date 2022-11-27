a5248c91ea281a4a9c78793e1a46447267991f43
Eunji Linda Lee <74166415+uxlindalee@users.noreply.github.com>
2022-11-27 22:43:38 +0900

    update

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,23 @@ import * as THREE from "../../lib/three.module.js";
 import { OrbitControls } from "../../lib/OrbitControls.js";
 import warehouseJson from "../../src/json/warehouse.json" assert { type: "json" };
 
+
+// Loader
+const loader = new THREE.FileLoader();
+loader.load("../../src/json/warehouse.json", function getData() {
+	if (window.warehouseDataGenerator) {
+		warehouseDataGenerator.getData();
+	}
+});
+window.dataCallback = function (data) {
+	if (data) {
+		let warehouse = data.warehouse;
+		let shuttle = data.shuttle;
+	}
+	// setTimeout(getData,50)
+};
+window.warehouseDataGenerator.getData();
+
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
 
@@ -14,32 +31,16 @@ const sizes = {
 	height: window.innerHeight,
 };
 
-// Loader
-const loader = new THREE.FileLoader();
-loader.load("../../src/json/warehouse.json", function getData() {
-	if (window.warehouseDataGenerator) {
-		warehouseDataGenerator.getData();
-	}
-});
-
-window.dataCallback = function (data) {
-	if (data) {
-		let warehouse = data.warehouse;
-		let shuttle = data.shuttle;
-	}
-	// setTimeout(getData,50)
-};
-
-window.warehouseDataGenerator.getData();
-
-//Group
+// Light
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.7);
+scene.add(ambientLight);
 
 // Axes Helper
 const axesHelper = new THREE.AxesHelper(5);
 scene.add(axesHelper);
 
-// Object
 
+// Object
 let boxSizes = warehouseJson.sizes;
 let boxRacks = warehouseJson.racks;
 let shuttle = boxSizes.shuttle;
@@ -53,8 +54,7 @@ let positionZ = 0;
 const buildWarehouse = function (d, h, w, x, y, z) {
 	const boxGroup = new THREE.Group();
 
-	// boxGroup.position.set(-5.8, 0, -1.15);
-	// boxGeometry.computeBoundingBox();
+	boxGroup.position.set(boxSizes.width.length * -1, 0, (boxSizes.depth.length + elevator.width * 0.001/2) * -0.3);
 
 	scene.add(boxGroup);
 
@@ -74,12 +74,26 @@ const buildWarehouse = function (d, h, w, x, y, z) {
 	boxGroup.add(box);
 };
 
+
 //Elevator
-buildWarehouse(elevator.depth * 0.001, elevator.height * 0.001, elevator.width * 0.001, -(elevator.width * 0.001) / 2, (elevator.height * 0.001) / 2, 0);
-// buildWarehouse(elevator.depth * 0.001, elevator.height * 0.001, elevator.width * 0.001, 0, 0, 0);
+buildWarehouse(
+	elevator.depth * 0.001, 
+	elevator.height * 0.001, 
+	elevator.width * 0.001, 
+	-(boxSizes.width[0] * 0.001 + (elevator.width * 0.001) / 2),
+	(elevator.height * 0.001) / 2, 
+	(elevator.width * 0.001) / 2 + 0.15
+);
 
 //Shuttle
-buildWarehouse(shuttle.depth * 0.001, shuttle.height * 0.001, shuttle.width * 0.001, -(shuttle.width * 0.001) / 2, (shuttle.height * 0.001) / 2, 0);
+buildWarehouse(
+	shuttle.depth * 0.001, 
+	shuttle.height * 0.001, 
+	shuttle.width * 0.001, 
+	-(boxSizes.width[0] * 0.001 + (elevator.width * 0.001) / 2),
+	(shuttle.height * 0.001) / 2,
+	(elevator.width * 0.001) / 2 + 0.15
+);
 
 //Containers in rack
 for (let k = 0; k < boxSizes.depth.length; k++) {
@@ -101,20 +115,62 @@ for (let k = 0; k < boxSizes.depth.length; k++) {
 	positionZ += boxSizes.depth[k] * 0.001 * 2 + shuttle.width * 0.001;
 }
 
-// Light
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.7);
-scene.add(ambientLight);
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0.2);
-directionalLight.castShadow = true;
-directionalLight.shadow.mapSize.set(1024, 1024);
-directionalLight.shadow.camera.far = 15;
-directionalLight.shadow.camera.left = -7;
-directionalLight.shadow.camera.top = 7;
-directionalLight.shadow.camera.right = 7;
-directionalLight.shadow.camera.bottom = -7;
-directionalLight.position.set(5, 5, 5);
-scene.add(directionalLight);
+
+// const setBoxes = function () {
+// 	let boxInfos = warehouseJson.box_infos;
+
+// 	for (let i = 0; i < warehouseData.warehouse.length; i++) {
+// 		const box = warehouseData.warehouse[i];
+// 		const boxInfo = boxInfos.find(item => item.type === box.box_type);
+// 		const boxIdArr = box.id.split('-');
+// 		const matchRack = warehouseRacks.find(item => item.ids === box.id.substring(0, 5));
+		
+// 		if ( boxInfo ) {
+// 			// ~~~~~~~~~~~~~~~~~~~ 이거 맞을까.......?............. 
+// 			if ( 'double' in boxInfo ) {
+// 				const boxMesh = [
+// 					createBox(
+// 						boxInfo.width/1000,
+// 						boxInfo.height/1000,
+// 						boxInfo.depth/1000,
+// 						matchRack.position.x - matchRack.geometry.parameters.width/2 + boxInfo.width/1000/2 + 0.15 + (boxIdArr[3]-1)*0.5, 
+// 						matchRack.position.y - matchRack.geometry.parameters.height/2 + boxInfo.height/1000/2,
+// 						matchRack.position.z - boxInfo.depth/1000/2 - 0.04,
+// 						boxInfo.color,
+// 						boxInfo.name,
+// 					),
+// 					createBox(
+// 						boxInfo.width/1000,
+// 						boxInfo.height/1000,
+// 						boxInfo.depth/1000,
+// 						matchRack.position.x - matchRack.geometry.parameters.width/2 + boxInfo.width/1000/2 + 0.15 + (boxIdArr[3]-1)*0.5, 
+// 						matchRack.position.y - matchRack.geometry.parameters.height/2 + boxInfo.height/1000/2,
+// 						matchRack.position.z + boxInfo.depth/1000/2 + 0.04,
+// 						boxInfo.color,
+// 						boxInfo.name,
+// 					)
+// 				]
+// 				warehouseBoxs.push(boxMesh);
+// 			} else {
+// 				const boxMesh = createBox(
+// 					boxInfo.width/1000,
+// 					boxInfo.height/1000,
+// 					boxInfo.depth/1000,
+// 					matchRack.position.x - matchRack.geometry.parameters.width/2 + boxInfo.width/1000/2 + 0.15 + (boxIdArr[3]-1)*0.5, 
+// 					matchRack.position.y - matchRack.geometry.parameters.height/2 + boxInfo.height/1000/2,
+// 					matchRack.position.z,
+// 					boxInfo.color,
+// 					boxInfo.name,
+// 				)
+// 				warehouseBoxs.push(boxMesh);
+// 			}
+// 		}
+// 	}
+// }
+
+
+
 
 window.addEventListener("resize", () => {
 	// Update sizes
@@ -132,11 +188,18 @@ window.addEventListener("resize", () => {
 
 // Camera
 const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0.1, 1000);
-camera.position.set(-20, 10, 20);
+camera.position.set(20, 10, -20);
+camera.lookAt(0, 0, 0);
 
 // Controls
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
+
+
+
+
+
+
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
